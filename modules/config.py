17b7b11e281ea2ddcbb330accf3165cdc35310a3
hemant67890 <godarasubhash337@gmail.com>
2025-03-01 06:36:26 +0530

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -1,7 +1,7 @@
 # config.py
 import os
-BOT_NAME = os.environ.get('BOT_NAME')
-BOT_TOKEN = os.environ.get('BOT_TOKEN')
-API_ID = int(os.environ.get('API_ID'))
-API_HASH = os.environ.get('API_HASH')
-MONGO_URI = os.environ.get('MONGO_URI')
+BOT_NAME = "My Bot"
+BOT_TOKEN = "7825685842:AAH8s89V6PCAU4R-KiHLwsiLOcDly0H5M0g"
+API_ID = 28328736
+API_HASH = "802254a44896baa87f3083b7af36b2e5"
+MONGO_URI = "mongodb+srv://mrnobody:modernhackers@mrnobody.q8e87ij.mongodb.net/?retryWrites=true&w=majority&appName=MrNobody"
